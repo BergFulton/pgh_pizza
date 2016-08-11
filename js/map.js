8c691661c0435068b196012a0b157fa43e7f5ecc
BergFulton <BergFulton@users.noreply.github.com>
2016-08-11 09:59:45 -0400

    WIP distance search is broken

--- js/map.js
+++ js/map.js
@@ -154,6 +154,11 @@ function initMap() {
                 hideMarkers(markers);
             });
 
+        //Directions search event listener
+        document.getElementById('search-within-time').addEventListener('click', function() {
+                searchWithinTime();
+            });
+
         //Listen for the event fired when the user selects a prediction 
         //and then clicks "go". Then get more details about the place
         document.getElementById('get-pizza').
@@ -254,3 +259,70 @@ function initMap() {
         }
     }
 }
+
+//This function allows the user to input a desired travel time and travel mode, and a location. It will only show places that are reachable within the desired travel duration period. 
+        function searchWithinTime() {
+            //Initialize the distance matrix service
+            var distanceMatrixServce = new google.maps.DistanceMatrixService;
+            var address = document.getElementById('search-within-time-text').value;
+            //Check to see that the field isn't blank
+            if (address == '') {
+                window.alert('Yinz gotta give me an address.');
+            } else {
+                hideMarkers(markers);
+                //Use the distance matrix service to calcuate the duration of the routes between all markers, and the destiation set by the user. The put all the origins into an origin matrix.
+                var origins = [];
+                for (var i = 0; i < markers.length; i++) {
+                    origins[i] = markers[i].position;
+                }
+                var destination = address;
+                var mode = document.getElementById('mode').value;
+                // Since we have defined the origin & destination, we'll
+                // get the info for the distances between them.
+                distanceMatrixServce.getDistanceMatrix({
+                    origins: origins,
+                    destinations: [destination],
+                    travelMode: google.maps.TravelMode[mode],
+                    unitSystem: google.maps.UnitSystem.IMPERIAL,
+                }, function(response, status) {
+                    if (status !== google.maps.DistanceMatrixStatus.OK) {
+                        window.alert('Error was: ' + status);
+                    } else {
+                        displayMarkersWithinTime(response);
+                    }
+                });
+            }
+        }
+
+
+        //This function is in reponse to the user selecting "show 
+        //route" on one of the markers iwthin the caluclated distance.
+        //This will display the route on the map
+        function displayDirections(origin) {
+            hideListings();
+            var directionsService = new google.maps.DirectionsService;
+            //Get the destination service address from the user input.
+            var destinationAddress = document.getElementById('search-within-time-text').value;
+            //Get mode of transportation from user input.
+            var mode = document.getElementById('mode').value;
+            directionsService.route({
+                //The origin is passed in marker's position.
+                origin: origin,
+                //The desitation is user entered address
+                destination: destinationAddress,
+                travelMode: google.maps.TravelMode[mode]
+            }, function(response, status) {
+                if (status === google.maps.DirectionsStatus.OK) {
+                    var directionsDisplay = new google.maps.DirectionsRenderer({
+                        map: map,
+                        directions: response,
+                        draggable: true,
+                        polylineOptions: {
+                            strokeColor: 'green'
+                        }
+                    });
+                } else {
+                    window.alert('Directions request failed due to ' + status);
+                }
+            });
+        }
